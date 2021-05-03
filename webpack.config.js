2a7638d00189f1fef85a4f090ae5451af52439b3
nabeliwo <nabeliwo@gmail.com>
2021-05-03 23:22:29 +0900

    prepare development environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { resolve } = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: resolve(__dirname, 'src/components.ts'),
+  output: {
+    filename: 'components.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+}
