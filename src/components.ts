211640296379024ea510230379e3c7c6d7174138
nabeliwo <nabeliwo@gmail.com>
2021-05-10 23:28:56 +0900

    add smarthr-ui button component

--- src/components.ts
+++ src/components.ts
@@ -3,7 +3,12 @@ import { NameTag } from './components/NameTag'
 import { MoreExpressions } from './components/MoreExpressions'
 import { ToDoList } from './components/ToDoList'
 
+import { Button } from './components/smarthr-ui/Button'
+import { AccordionPanel } from './components/smarthr-ui/AccordionPanel'
+
 customElements.define('my-element', MyElement)
 customElements.define('name-tag', NameTag)
 customElements.define('more-expressions', MoreExpressions)
 customElements.define('todo-list', ToDoList)
+customElements.define('smarthr-ui-button', Button)
+customElements.define('accordion-panel', AccordionPanel)
---
+++ src/components/smarthr-ui/AccordionPanel/AccordionPanel.ts
@@ -0,0 +1,63 @@
+import { LitElement, css, html } from 'lit'
+
+export class AccordionPanel extends LitElement {
+  theme
+
+  static get properties() {
+    return {
+      theme: {},
+    }
+  }
+
+  constructor() {
+    super()
+    this.theme = 'primary'
+  }
+
+  render() {
+    return html`
+      <button class="button ${this.theme}">
+        <slot></slot>
+      </button>
+    `
+  }
+
+  static get styles() {
+    return css`
+      .button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        height: 40px;
+        padding: 0px 16px;
+        border-radius: 6px;
+        border: none;
+        font-weight: bold;
+        font-size: 14px;
+        text-align: center;
+        vertical-align: middle;
+        text-decoration: none;
+        box-sizing: border-box;
+        cursor: pointer;
+      }
+      .button:focus {
+        outline: none;
+        box-shadow: #fff 0 0 0 2px, #0077c7 0 0 0 4px;
+      }
+      .primary {
+        background-color: #0077c7;
+        color: #fff;
+      }
+      .primary:hover {
+        background-color: #0068ae;
+      }
+      .danger {
+        background-color: #e01e5a;
+        color: #fff;
+      }
+      .danger:hover {
+        background-color: #ca1b51;
+      }
+    `
+  }
+}
